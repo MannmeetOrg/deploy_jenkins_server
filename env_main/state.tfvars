609d91356327a00575be5922329db6d6f19aa941
MannuA19 <mannmeet.ahir@gmail.com>
2025-07-05 21:45:02 +0530

    Github Workflow

--- env_main/state.tfvars
+++
@@ -1,3 +0,0 @@
-bucket = "jenkins-server-ec2"
-key    = "jenkins/terraform.tfstate"
-region = "us-east-1"
--- state.tf
+++ state.tf
@@ -1,3 +1,7 @@
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket = "jenkins-server-ec2"
+    key    = "jenkins/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
