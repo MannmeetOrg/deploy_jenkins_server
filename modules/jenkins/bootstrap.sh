229e8032a20a59e1b5e7d6a6b11938d0498c24ce
MannuA19 <mannmeet.ahir@gmail.com>
2025-07-06 22:10:13 +0530

    Github Workflow

--- modules/jenkins/bootstrap.sh
+++ modules/jenkins/bootstrap.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 sudo yum update -y
-sudo yum install -y java-11-openjdk
+#JAVA Install
+sudo yum install -y java-11-amazon-corretto
+java -version
+
+# JENKINS INSTALL
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 sudo yum upgrade -y
