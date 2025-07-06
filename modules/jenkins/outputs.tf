3b4b89e8df3db1f64326b45cfe159dfa409b69b6
MannuA19 <mannmeet.ahir@gmail.com>
2025-07-06 16:05:56 +0530

    Terraform Code Updated

--- modules/jenkins/outputs.tf
+++ modules/jenkins/outputs.tf
@@ -2,21 +2,9 @@ output "public_ip" {
   value = aws_instance.jenkins.public_ip
 }
 
-
-output "instance_type" {
-  value = aws_instance.jenkins.instance_type
+output "private_ip" {
+  value = aws_instance.jenkins.private_ip
 }
-
-output "ami_id" {
-  value = aws_instance.jenkins.id
-}
-
-output "subnet_id" {
-  value = aws_instance.jenkins.subnet_id
-}
-
-output "sg_id" {
-  value = aws_instance.jenkins.security_groups
 }
 
 
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "jenkins_public_ip" {
-  value = aws_instance.jenkins.public_ip
+  value = module.jenkins.public_ip
+}
+
+output "jenkins_private_ip" {
+  value = module.jenkins.private_ip
 }
