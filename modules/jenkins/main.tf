99bd433c9fed4594f10ce02882fcbec0f4ebce52
MannuA19 <mannmeet.ahir@gmail.com>
2025-07-11 21:03:56 +0530

    Terraform Code Updated

--- modules/jenkins/main.tf
+++ modules/jenkins/main.tf
@@ -88,6 +88,22 @@ resource "aws_security_group" "jenkins_sg" {
     self      = true
   }
 
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
